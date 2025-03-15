5f123a37980d6ac47b190e8030e61b4450e9b16b
Sunghyun Cho <hey@cho.sh>
2025-03-15 23:47:45 +0900

    refactor: Update NixOS configuration description and inputs

--- flake.nix
+++ flake.nix
@@ -1,12 +1,20 @@
 {
-  description = "Sunghyun Cho's NixOS";
+  description = "NixOS Configuration for sunghyuncho";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-  outputs = { self, nixpkgs, ... }: {
-    nixosConfigurations.sunghyuncho = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
-    };
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable"; 
   };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.sunghyuncho = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
 }
