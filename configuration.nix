0f6bb4a31681c1311d10b76a42e0d20be37ab765
Sunghyun Cho <hey@cho.sh>
2025-03-16 01:00:18 +0900

    feat: Add systemd service for flatpak repository

--- configuration.nix
+++ configuration.nix
@@ -74,6 +74,15 @@
 
   services.flatpak.enable = true;
   services.flatpak.registry.flathub.enable = true;
+
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+
   services.flatpak.packages = [
     "com.spotify.Client"
     "com.1password.1Password"
