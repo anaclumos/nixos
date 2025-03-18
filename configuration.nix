c5dd0309e58cab1b29a03d1464ec1f25c3fc189c
Sunghyun Cho <hey@cho.sh>
2025-03-19 01:39:37 +0900

    Added IME

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 {
-  imports = [ ./hardware-configuration.nix ];
+  imports = [ 
+    ./hardware-configuration.nix
+  ];
   
   # Bootloader
   boot.loader.systemd-boot.enable = true;
@@ -11,26 +13,34 @@
   
   # Time zone and locale
   time.timeZone = "Asia/Seoul";
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+    inputMethod = {
+      enabled = "ibus";
+      ibus.engines = with pkgs.ibus-engines; [ hangul ];
+    };
   };
   
   # X11 and Desktop Environment
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.xkb = {
-    layout = "kr";
-    variant = "";
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+    xkb = {
+      layout = "us";  
+      variant = "";
+    };
   };
   
   # Printing
@@ -61,6 +71,7 @@
   environment.systemPackages = with pkgs; [
     zsh
     git
+    home-manager
   ];
   
   # Enable zsh system-wide
--- home.nix
+++ home.nix
@@ -3,9 +3,46 @@
   home.username = "sunghyuncho";
   home.homeDirectory = "/home/sunghyuncho";
   
+  # Keyboard remapping configuration
+  home.keyboard = {
+    layout = "us";
+    options = [
+      "caps:super"     # Caps Lock → Super
+      "ctrl:swap_lwin_lctl"  # Left Win/Cmd → Left Ctrl
+    ];
+  };
+
+  # X11 key bindings for terminal copy/paste and input method switching
+  xsession.windowManager.command = ''
+    ${pkgs.xorg.xmodmap}/bin/xmodmap - <<EOF
+      keycode 133 = Control_L
+      keycode 134 = Control_L
+      add Control = Control_L
+    EOF
+
+    # Configure IBus
+    export GTK_IM_MODULE=ibus
+    export XMODIFIERS=@im=ibus
+    export QT_IM_MODULE=ibus
+    ${pkgs.ibus}/bin/ibus-daemon -drx
+  '';
+
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       clock-format = "12h";
+    };
+    # Terminal keybindings for GNOME Terminal
+    "org/gnome/terminal/legacy/keybindings" = {
+      copy = "<Primary>c";
+      paste = "<Primary>v";
+    };
+    # IBus preferences
+    "desktop/ibus/general" = {
+      preload-engines = ["hangul"];
+      use-system-keyboard-layout = true;
+    };
+    "desktop/ibus/general/hotkey" = {
+      triggers = ["Super_L" "Super_R"];  # Left and Right Command keys
     };
   };
   
@@ -21,6 +58,10 @@
     gh
     (lib.hiPrio windsurf)
     nodejs
+    pnpm
+    slack
+    ibus
+    ibus-engines.hangul
   ];
   
   programs.home-manager.enable = true;  
