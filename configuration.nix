74559171f96c33ed844b31c3b2ddcf17450eb9f2
Sunghyun Cho <hey@cho.sh>
2025-03-19 10:20:17 +0900

    250319

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, ... }: {
   imports = [ ./hardware-configuration.nix ];
 
   # Bootloader
@@ -45,7 +45,7 @@
   services.printing.enable = true;
 
   # Audio
-  services.pulseaudio.enable = false;
+  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -60,26 +60,79 @@
     description = "sunghyuncho";
     extraGroups = [ "networkmanager" "wheel" ];
     shell = pkgs.zsh;
+    packages = with pkgs; [
+      git
+      vscode
+      google-chrome
+      gitAndTools.hub
+      spotify
+      asdf-vm
+      _1password-cli
+      _1password-gui
+      gh
+      nodejs
+      pnpm
+      slack
+      ibus
+      ibus-engines.hangul
+      nixfmt-classic
+      warp-terminal
+    ];
   };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   # System packages
-  environment.systemPackages = with pkgs; [ zsh git home-manager ];
+  environment.systemPackages = with pkgs; [ zsh git ];
 
   # Enable zsh system-wide
-  programs.zsh.enable = true;
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+    ohMyZsh = {
+      enable = true;
+      plugins = [ "git" "docker" "npm" "sudo" "command-not-found" "zsh-autosuggestions" ];
+      theme = "robbyrussell";
+    };
+    shellAliases = {
+      rebuild = "sudo nix flake update && sudo nixos-rebuild switch";
+    };
+  };
+
+  # Git configuration
+  programs.git = {
+    enable = true;
+    config = {
+      user.name = "Sunghyun Cho";
+      user.email = "hey@cho.sh";
+    };
+  };
+
+  # SSH configuration with 1Password
+  programs.ssh = {
+    startAgent = true;
+    extraConfig = ''
+      Host *
+        IdentityAgent ~/.1password/agent.sock
+    '';
+  };
 
   # Enable Flatpak
   services.flatpak.enable = true;
 
   # Fonts
   fonts.packages = with pkgs; [ pretendard ];
-
   fonts.fontconfig.defaultFonts = {
     serif = [ "Pretendard" ];
     sansSerif = [ "Pretendard" ];
+  };
+
+  # PNPM configuration through environment variables
+  environment.sessionVariables = {
+    PNPM_HOME = "/root/.local/share/pnpm";
+    PATH = [ "\${PNPM_HOME}" ];
   };
 
   system.stateVersion = "24.11";
--- flake.nix
+++ flake.nix
@@ -4,25 +4,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     _1password-shell-plugins.url = "github:1Password/shell-plugins";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, ... }: {
     nixosConfigurations.spaceship = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.sunghyuncho = import ./home.nix;
-          };
-        }
       ];
     };
   };
