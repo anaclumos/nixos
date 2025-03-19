b9d61cd7dcec5474fab55906067ee4fda9424786
Sunghyun Cho <hey@cho.sh>
2025-03-19 12:47:49 +0900

    250319

--- flake.nix
+++ flake.nix
@@ -1,12 +1,29 @@
 {
   description = "Sunghyun Cho's NixOS Configuration";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
 
-  outputs = { self, nixpkgs, ... }: {
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    _1password-shell-plugins.url = "github:1Password/shell-plugins";
+  };
+
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.spaceship = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.users.sunghyuncho = import ./home.nix;
+        }
+      ];
     };
   };
 }
