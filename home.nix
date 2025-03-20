ccd255806a3f2d9377b2bf9a09ad3bd515f1abe3
Sunghyun Cho <hey@cho.sh>
2025-03-20 13:36:01 +0900

    2025-03-20

--- home.nix
+++ home.nix
@@ -39,6 +39,7 @@
     adguardhome
     beeper
     xclip
+    fastfetch
   ];
 
   programs._1password-shell-plugins = {
@@ -59,9 +60,11 @@
     };
 
     shellAliases = {
-      rebuild = ''find . -name "*.nix" -type f | xargs nixfmt && sudo nix flake update && sudo nixos-rebuild switch'';
+      rebuild = ''
+        find . -name "*.nix" -type f | xargs nixfmt && sudo nix flake update && sudo nixos-rebuild switch'';
       nixgit = ''git commit -m "$(date +"%Y-%m-%d")" -a && git push'';
-      llm = ''find . -type f ! -path "*/.git/*" ! -name "*.lock*" ! -name "*lock.*" -exec grep -Iq . {} \; -and -exec sh -c 'echo -e "### $(basename $1)\n\n\`\`\`\n$(cat $1)\n\`\`\`\n\n"' sh {} \; | xclip -selection clipboard'';
+      llm = ''
+        find . -type f ! -path "*/.git/*" ! -name "*.lock*" ! -name "*lock.*" -exec grep -Iq . {} \; -and -exec sh -c 'echo -e "### $(basename $1)\n\n\`\`\`\n$(cat $1)\n\`\`\`\n\n"' sh {} \; | xclip -selection clipboard'';
     };
 
     initExtra = ''
@@ -72,6 +75,9 @@
       if [ -z "$(ssh-add -l 2>/dev/null)" ]; then
         op signin
       fi
+
+      # Run fastfetch on terminal start
+      command -v fastfetch >/dev/null 2>&1 && fastfetch
     '';
   };
 
