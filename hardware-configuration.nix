65ceb920e39af664b51547569f83812466230306
Sunghyun Cho <hey@cho.sh>
2025-05-19 20:50:24 +0900

    Add MacBook

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -4,21 +4,26 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+  imports = [
+    (modulesPath + "/hardware/network/broadcom-43xx.nix")
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules =
+    [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
+
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/17d09a00-d1b0-44d3-a6df-a2416ec420ca";
+    device = "/dev/disk/by-uuid/c1f225fb-0047-4926-976e-14380542e8cf";
     fsType = "ext4";
   };
 
-  # EFI/boot partition on /dev/nvme0n1p1
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/3432-CAD0";
+    device = "/dev/disk/by-uuid/62C2-F424";
     fsType = "vfat";
+    options = [ "fmask=0077" "dmask=0077" ];
   };
 
   swapDevices = [ ];
@@ -27,8 +32,7 @@
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
+  networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
