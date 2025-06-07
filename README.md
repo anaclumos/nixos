b7e2be3564448c4226530e99c11c8d85e9664052
Sunghyun Cho <hey@cho.sh>
2025-06-07 19:02:23 -0400

    Reset for Framework Laptop

--- README.md
+++
@@ -1,6 +0,0 @@
-## Sunghyun's NixOS
-
-```bash
-sudo nixos-rebuild switch
-```
-
--- configuration.nix
+++ configuration.nix
@@ -1,97 +1,50 @@
-{ config, pkgs, lib, ... }: {
-  imports = [ ./hardware-configuration.nix ];
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  security.polkit.enable = true;
-  security.rtkit.enable = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "spaceship";
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 22 ];
-    trustedInterfaces = [ "tailscale0" ];
-    allowedUDPPorts = [ config.services.tailscale.port ];
+  services.fwupd.enable = true;
+  services.fprintd.enable = true;
+
+  networking.hostName = "cho";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/New_York";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
-  nixpkgs.config.allowUnfree = true;
-  xdg.icons.enable = true;
-  programs.gdk-pixbuf.modulePackages = [ pkgs.librsvg ];
+  services.xserver.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.pantheon.enable = true;
 
-  environment.sessionVariables = {
-    XDG_DATA_DIRS = lib.mkForce (lib.concatStringsSep ":" [
-      "${pkgs.glib}/share"
-      "/run/current-system/sw/share"
-      "/home/sunghyuncho/.local/share"
-    ]);
-    PNPM_HOME = "/root/.local/share/pnpm";
-    PATH = [ "/root/.local/share/pnpm" ];
-    MUTTER_ALLOW_CUSTOM_SCALING_FACTORS = "1";
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
   };
 
-  time.timeZone = "Asia/Seoul";
-  time.hardwareClockInLocalTime = true;
-
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocales = [ "ko_KR.UTF-8" ];
-    inputMethod = {
-      type = "ibus";
-      enable = true;
-      ibus.engines = with pkgs.ibus-engines; [ hangul ];
-    };
-  };
-
-  services.xserver = {
-    enable = true;
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
-    xkb = { layout = "us"; };
-  };
-
-  services.xserver.desktopManager.gnome.extraGSettingsOverrides = ''
-    [org.gnome.desktop.interface]
-    gtk-theme='WhiteSur-Dark'
-    icon-theme='WhiteSur'
-
-    [org.gnome.desktop.wm.preferences]
-    button-layout='appmenu:minimize,maximize,close'
-
-    [org.gnome.desktop.background]
-    show-desktop-icons=true
-  '';
-
-  virtualisation.docker = {
-    enable = true;
-    enableOnBoot = true;
-  };
-
-  services.displayManager.hiddenUsers = [ "root" ];
-
-  services.libinput = {
-    enable = true;
-
-    touchpad = {
-      naturalScrolling = true;
-      scrollMethod = "twofinger";
-      accelSpeed = "0.5";
-      accelProfile = "adaptive";
-    };
-
-    mouse = {
-      accelProfile = "flat";
-      middleEmulation = false;
-    };
-  };
-
-  hardware.logitech.wireless = {
-    enable = true;
-    enableGraphical = true;
-  };
+  services.printing.enable = true;
 
   services.pulseaudio.enable = false;
-
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
@@ -99,93 +52,61 @@
     pulse.enable = true;
   };
 
-  services.printing.enable = true;
-
-  services.tailscale = {
-    enable = true;
-    openFirewall = true;
-    authKeyFile = null;
-    extraUpFlags = [ "--ssh" ];
-  };
-
-  networking.networkmanager = { enable = true; };
-
-  users.users.sunghyuncho = {
+  users.users.sunghyun = {
     isNormalUser = true;
-    description = "성현";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    description = "Sunghyun Cho";
+    extraGroups = [ "networkmanager" "wheel" ];
     shell = pkgs.zsh;
-    packages = with pkgs; [
-      git
-      gitAndTools.hub
-      asdf-vm
-      nodejs
-      nodePackages.pnpm
-      bun
-      nixfmt-classic
-      claude-code
-      slack
-      obsidian
-      ibus
-      ibus-engines.hangul
-      google-chrome
-      windsurf
-      adguardhome
-      xclip
-      fastfetch
-      tailscale
-      bottles
-      steam
-      google-cloud-sdk
-      pretendard
-    ];
+    packages = with pkgs; [ ];
   };
 
-  programs.zsh.enable = true;
-  programs._1password = { enable = true; };
-  programs._1password-gui = {
+  
+  programs.git = {
     enable = true;
-    polkitPolicyOwners = [ "sunghyuncho" ];
-  };
-  programs.nix-ld.enable = true;
-  programs.ssh.startAgent = false;
-
-  environment.etc = {
-    "xdg/autostart/1password.desktop" = {
-      source = "${pkgs._1password-gui}/share/applications/1password.desktop";
-      mode = "0644";
+    config = {
+      user.name = "Sunghyun Cho";
+      user.email = "hey@cho.sh";
+      gpg.format = "ssh";
+      gpg.ssh.program = "${pkgs._1password-gui}/share/1password/op-ssh-sign";
+      commit.gpgsign = true;
+      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGaWDMcfAJMbWDorZP8z1beEAz+fjLb+VFqFm8hkAlpt";
     };
-    "AccountsService/users/root".text = ''
-      [User]
-      SystemAccount=true
-    '';
   };
+  
+  programs.zsh.enable = true;
 
-  # Additional setting to hide root user
-  users.users.root.isSystemUser = true;
+  nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
-    # GNOME packages
-    gnome-tweaks
-    dconf-editor
-    whitesur-gtk-theme
-    whitesur-icon-theme
-    hicolor-icon-theme
-    adwaita-icon-theme
-
-    # System packages
-    uv
+    git
+    zsh
+    zsh-autosuggestions
+    gitAndTools.hub
+    asdf-vm
+    nodejs
+    nodePackages.pnpm
+    bun
+    nixfmt-classic
+    claude-code
+    slack
+    obsidian
+    ibus
+    ibus-engines.hangul
+    google-chrome
+    windsurf
+    adguardhome
+    xclip
+    fastfetch
+    tailscale
+    bottles
+    steam
+    google-cloud-sdk
+    pretendard
+    vscode
     _1password-gui
     _1password-cli
-    docker-compose
-    solaar
   ];
 
-  fonts.packages = with pkgs; [ pretendard ];
-  fonts.fontconfig.defaultFonts = {
-    serif = [ "Pretendard" ];
-    sansSerif = [ "Pretendard" ];
-  };
-
-  system.stateVersion = "24.11";
+  system.stateVersion = "25.05";
 }
+
