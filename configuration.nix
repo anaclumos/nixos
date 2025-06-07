084f77e77b1221db0d608bf897651592f1e714e6
Sunghyun Cho <hey@cho.sh>
2025-06-07 19:50:29 -0400

    Add IBUS new methods

--- configuration.nix
+++ configuration.nix
@@ -1,9 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-  ];
+  imports = [ ./hardware-configuration.nix ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -17,19 +15,34 @@
 
   time.timeZone = "America/New_York";
   i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "ko_KR.UTF-8";
+    LC_IDENTIFICATION = "ko_KR.UTF-8";
+    LC_MEASUREMENT = "ko_KR.UTF-8";
+    LC_MONETARY = "ko_KR.UTF-8";
+    LC_NAME = "ko_KR.UTF-8";
+    LC_NUMERIC = "ko_KR.UTF-8";
+    LC_PAPER = "ko_KR.UTF-8";
+    LC_TELEPHONE = "ko_KR.UTF-8";
+    LC_TIME = "ko_KR.UTF-8";
+  };
+
   i18n.inputMethod = {
-    type = "ibus";
     enable = true;
+    type = "ibus";
     ibus.engines = with pkgs.ibus-engines; [ hangul ];
   };
+
+  fonts.packages = with pkgs; [ nanum noto-fonts-cjk-sans pretendard ];
 
   services.xserver = {
     enable = true;
     displayManager.lightdm.enable = true;
     desktopManager.pantheon.enable = true;
     xkb = {
-      layout = "us";
-      variant = "";
+      layout = "us,kr";
+      variant = ",kr104";
+      options = "grp:alt_space_toggle";
     };
   };
 
@@ -56,12 +69,7 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [
-    git
-    zsh
-    ibus
-    ibus-engines.hangul
-  ];
+  environment.systemPackages = with pkgs; [ git zsh ibus ibus-engines.hangul ];
 
   system.stateVersion = "25.05";
 }
