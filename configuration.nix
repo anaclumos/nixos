3caf7836ccb6cf4025770ab7eaee4f59596f57ee
Sunghyun Cho <hey@cho.sh>
2025-06-07 22:07:05 -0400

    2025-06-07

--- configuration.nix
+++ configuration.nix
@@ -132,7 +132,7 @@
 
     shellAliases = {
       rebuild =
-        "cd ~/Desktop/nixos && nixfmt *.nix && nix-channel --update && nix --extra-experimental-features 'nix-command flakes' flake update && sudo nixos-rebuild switch --flake .#spaceship";
+        "cd ~/Desktop/nixos && nixfmt *.nix && nix-channel --update && nix --extra-experimental-features 'nix-command flakes' flake update && sudo nixos-rebuild switch --flake .#cho";
       nixgit = ''git commit -m "$(date +"%Y-%m-%d")" -a && git push'';
       qqqq = "cd ~/Desktop/extracranial && bun run save";
       x = "exit";
