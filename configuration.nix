d4e2a25d52dc7a55ae62239bba637b432dbfdf33
Sunghyun Cho <hey@cho.sh>
2025-06-09 16:24:03 -0400

    feat: add GNOME package exclusions to configuration.nix for a cleaner desktop environment

--- configuration.nix
+++ configuration.nix
@@ -29,6 +29,36 @@
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
 
+  environment.gnome.excludePackages = with pkgs.gnome; [
+    baobab # disk usage analyzer
+    cheese # photo booth
+    eog # image viewer
+    epiphany # web browser
+    gedit # text editor
+    simple-scan # document scanner
+    totem # video player
+    yelp # help viewer
+    evince # document viewer
+    file-roller # archive manager
+    geary # email client
+    seahorse # password manager
+    gnome-calculator
+    gnome-calendar
+    gnome-characters
+    gnome-clocks
+    gnome-contacts
+    gnome-font-viewer
+    gnome-logs
+    gnome-maps
+    gnome-music
+    gnome-photos
+    gnome-screenshot
+    gnome-system-monitor
+    gnome-weather
+    gnome-disk-utility
+    pkgs.gnome-connections
+  ];
+
   # Audio
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
