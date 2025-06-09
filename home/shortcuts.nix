384941ab44a263fedc15cafd6fd3898ce73be760
Sunghyun Cho <hey@cho.sh>
2025-06-09 14:56:16 -0400

    fix: wrap commands in bash for custom keybindings in shortcuts.nix

--- home/shortcuts.nix
+++ home/shortcuts.nix
@@ -10,12 +10,12 @@
     };
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       name = "Launch or Focus Chrome";
-      command = "wmctrl -x -a google-chrome.Google-chrome || google-chrome-stable";
+      command = "bash -c 'wmctrl -x -a google-chrome.Google-chrome || google-chrome-stable'";
       binding = "<Ctrl><Alt><Super><Shift>j";
     };
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
       name = "Launch or Focus Obsidian";
-      command = "wmctrl -x -a obsidian.Obsidian || obsidian";
+      command = "bash -c 'wmctrl -x -a obsidian.Obsidian || obsidian'";
       binding = "<Ctrl><Alt><Super><Shift>o";
     };
   };
