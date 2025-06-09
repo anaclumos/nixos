a1d11917b5de15fa17958de912775fca0585980b
Sunghyun Cho <hey@cho.sh>
2025-06-09 09:56:19 -0400

    Refactor imports formatting in configuration.nix for consistency

--- configuration.nix
+++ configuration.nix
@@ -1,10 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-  ./hardware-configuration.nix
-  ./keyboard.nix
-  ];
+  imports = [ ./hardware-configuration.nix ./keyboard.nix ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
