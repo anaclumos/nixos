aaf9ad425a60d8cd2c8e03dc004e27d1b69d1cec
Sunghyun Cho <hey@cho.sh>
2025-06-09 14:49:27 -0400

    refactor: restructure home configuration by splitting into multiple files and removing sunghyun.nix

---
+++ home/locale.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+
+{
+  home.language = {
+    base = "en_US.UTF-8";
+    address = "en_US.UTF-8";
+    measurement = "en_US.UTF-8";
+    monetary = "en_US.UTF-8";
+    time = "en_US.UTF-8";
+  };
+}
