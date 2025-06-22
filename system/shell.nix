a727366acf54e84771e7754e0860da17b36d3c90
Sunghyun Cho <hey@cho.sh>
2025-06-22 16:55:33 +0900

    fix: update shell aliases to reflect new directory structure

--- system/shell.nix
+++ system/shell.nix
@@ -20,7 +20,7 @@
 
     shellAliases = {
       rebuild =
-        "cd ~/Desktop/nixos && nixfmt **/*.nix && nix-channel --update && nix --extra-experimental-features 'nix-command flakes' flake update && sudo NIXPKGS_ALLOW_UNFREE=1 nixos-rebuild switch --flake .#cho --impure";
+        "cd ~/Desktop/nix/os && nixfmt **/*.nix && nix-channel --update && nix --extra-experimental-features 'nix-command flakes' flake update && sudo NIXPKGS_ALLOW_UNFREE=1 nixos-rebuild switch --flake .#cho --impure";
       nixgit = ''git commit -m "$(date +"%Y-%m-%d")" -a && git push'';
       qq = "cd ~/Desktop/extracranial";
       qqqq = "cd ~/Desktop/extracranial && bun run save && gp && exit";
@@ -28,7 +28,7 @@
       ed = "expressvpn disconnect";
       x = "exit";
       oo = "hub browse";
-      zz = "cursor ~/Desktop/nixos";
+      zz = "cursor ~/Desktop/nix/os";
       ss = "source ~/.zshrc";
       cc = "cursor .";
       sha =
