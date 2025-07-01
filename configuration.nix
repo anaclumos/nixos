32e041cdfaf783910bcd89b1cab11938f467651e
Sunghyun Cho <hey@cho.sh>
2025-07-01 11:24:42 +0900

    chore: comment out unused GNOME packages in configuration.nix and update flake.lock with new lastModified timestamps and narHashes

--- configuration.nix
+++ configuration.nix
@@ -49,30 +49,30 @@
   services.tailscale.enable = true;
 
   environment.gnome.excludePackages = with pkgs; [
-    baobab # disk usage analyzer
-    cheese # photo booth
-    eog # image viewer
+    # baobab # disk usage analyzer
+    # cheese # photo booth
+    # eog # image viewer
     epiphany # web browser
     gedit # text editor
-    simple-scan # document scanner
+    # simple-scan # document scanner
     # totem # video player
     yelp # help viewer
     evince # document viewer
-    file-roller # archive manager
+    # file-roller # archive manager
     geary # email client
     seahorse # password manager
-    gnome-calculator
+    # gnome-calculator
     # gnome-calendar
-    gnome-characters
-    gnome-clocks
+    # gnome-characters
+    # gnome-clocks
     gnome-contacts
-    gnome-font-viewer
-    gnome-logs
+    # gnome-font-viewer
+    # gnome-logs
     gnome-maps
     gnome-music
     gnome-photos
-    gnome-screenshot
-    gnome-system-monitor
+    # gnome-screenshot
+    # gnome-system-monitor
     gnome-weather
     # gnome-disk-utility
     pkgs.gnome-connections
