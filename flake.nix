07d18d71f08efef4dbb340e2f2c23bf42ef5270c
Sunghyun Cho <hey@cho.sh>
2025-07-04 23:27:55 +0900

    chore: update flake.lock with new lastModified timestamps and narHashes; modify flake.nix for consistency in input handling

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
   };
 
   outputs =
-  { self, nixpkgs, home-manager, nixos-hardware, kakaotalk, ... }@inputs: {
+    { self, nixpkgs, home-manager, nixos-hardware, kakaotalk, ... }@inputs: {
       nixosConfigurations = {
         cho = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
