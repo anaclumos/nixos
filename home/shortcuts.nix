a430dc08e07d3fdafdce16183436cf79c13fc95b
Sunghyun Cho <hey@cho.sh>
2025-07-07 16:17:01 +0900

    chore: update keybinding commands in home/shortcuts.nix to open Thunderbird instead of Notion Mail and Calendar

--- home/shortcuts.nix
+++ home/shortcuts.nix
@@ -68,13 +68,13 @@
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom7" =
       {
         name = "Open Notion Calendar";
-        command = "xdg-open https://calendar.notion.so";
+        command = "wmctrl -x -a Mail.thunderbird || thunderbird";
         binding = "<Ctrl><Alt><Super><Shift>l";
       };
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom8" =
       {
         name = "Open Notion Mail";
-        command = "xdg-open https://mail.notion.so";
+        command = "wmctrl -x -a Mail.thunderbird || thunderbird";
         binding = "<Ctrl><Alt><Super><Shift>h";
       };
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom9" =
