8f91ef94fa01feda936ed2ba6dd08cec390735bb
Sunghyun Cho <hey@cho.sh>
2025-07-08 17:01:45 +0900

    Delete Chrome Default Settings

--- home/programs.nix
+++ home/programs.nix
@@ -18,17 +18,4 @@
     };
 
   };
-
-  # Set Google Chrome as the default browser
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "text/html" = [ "google-chrome.desktop" ];
-      "x-scheme-handler/http" = [ "google-chrome.desktop" ];
-      "x-scheme-handler/https" = [ "google-chrome.desktop" ];
-      "x-scheme-handler/about" = [ "google-chrome.desktop" ];
-      "x-scheme-handler/unknown" = [ "google-chrome.desktop" ];
-    };
-  };
-
 }
