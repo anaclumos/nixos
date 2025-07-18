a66d23abb531dc71624e0d6441e76ef8274baa30
Sunghyun Cho <hey@cho.sh>
2025-07-18 15:48:42 +0900

    2025-07-18

--- gnome/gnome-appearance.nix
+++ gnome/gnome-appearance.nix
@@ -1,10 +1,17 @@
 { pkgs, ... }:
 
 {
-  home.packages = [ pkgs.whitesur-icon-theme pkgs.hicolor-icon-theme ];
+  home.packages = [
+    pkgs.whitesur-icon-theme
+    pkgs.hicolor-icon-theme
+    pkgs.adwaita-icon-theme
+  ];
 
   dconf.settings = {
     "org/gnome/desktop/interface" = {
+      show-battery-percentage = true;
+      font-name = "Pretendard 11";
+      monospace-font-name = "Berkeley Mono 10";
       icon-theme = "WhiteSur-dark";
       clock-show-weekday = true;
       clock-format = "12h";
