66f89da7d5d412ce3c4978ff976cf8e48649d863
Sunghyun Cho <hey@cho.sh>
2025-07-24 12:08:12 +0900

    2025-07-24

--- home/gcloud.nix
+++ home/gcloud.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    (google-cloud-sdk.withExtraComponents [
-      google-cloud-sdk.components.gke-gcloud-auth-plugin
-    ])
-  ];
+  home.packages = with pkgs;
+    [
+      (google-cloud-sdk.withExtraComponents
+        [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
+    ];
 }
