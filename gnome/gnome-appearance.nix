32c7ef969ad6e5cc5ad28e1be9d647b54282515c
Sunghyun Cho <hey@cho.sh>
2025-07-31 16:47:05 +0900

    2025-07-31

--- gnome/gnome-appearance.nix
+++ gnome/gnome-appearance.nix
@@ -12,7 +12,7 @@
       show-battery-percentage = true;
       font-name = "Pretendard 11";
       monospace-font-name = "Berkeley Mono 10";
-      icon-theme = "WhiteSur-dark";
+      icon-theme = "WhiteSur";
       clock-show-weekday = true;
       clock-format = "12h";
       enable-animations = false;
