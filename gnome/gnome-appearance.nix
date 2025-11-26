5804a55013c4a5d82eb9c097811a680469d6d606
Sunghyun Cho <hey@cho.sh>
2025-11-26 19:28:18 +0900

    Update NixOS configuration files and UUIDs
    - Updated disk UUIDs in hardware-configuration.nix for root and boot partitions.
    - Modified GNOME appearance settings in gnome-appearance.nix to include document font and titlebar font.
    - Adjusted service paths in gnome-desktop.nix for clarity.
    - Updated lastModified timestamps and narHashes in flake.lock for dependencies.

--- gnome/gnome-appearance.nix
+++ gnome/gnome-appearance.nix
@@ -11,12 +11,20 @@
     "org/gnome/desktop/interface" = {
       show-battery-percentage = true;
       font-name = "Pretendard 12";
+      document-font-name = "Pretendard 12";
       monospace-font-name = "Berkeley Mono 12";
       icon-theme = "Adwaita";
       cursor-theme = "elementary";
       clock-show-weekday = true;
       clock-format = "12h";
+      enable-hot-corners = false;
       # enable-animations = false;
     };
+
+    "org/gnome/desktop/wm/preferences" = {
+      titlebar-font = "Pretendard Bold 12";
+    };
+
+    "org/gnome/system/locale" = { region = "en_US.UTF-8"; };
   };
 }
