1eef40560e832fb66c17804aca7dbcccbeb46926
Sunghyun Cho <hey@cho.sh>
2025-11-26 23:53:35 +0900

    Enhance GNOME extensions configuration and update flake.lock
    - Added new dconf settings for GNOME extensions in gnome-extensions.nix to enable specific extensions.
    - Updated lastModified timestamp and narHash in flake.lock to reflect recent changes in dependencies.

--- gnome/gnome-extensions.nix
+++ gnome/gnome-extensions.nix
@@ -6,8 +6,20 @@
     clipboard-history
     auto-power-profile
     appindicator
-    unite
     kimpanel
     dock-from-dash
   ];
+
+  dconf.settings = {
+    "org/gnome/shell" = {
+      enabled-extensions = [
+        "gtk4-ding@smedius.gitlab.com"
+        "clipboard-history@alexsaveau.dev"
+        "auto-power-profile@dmy3k.github.io"
+        "appindicatorsupport@rgcjonas.gmail.com"
+        "kimpanel@kde.org"
+        "dock-from-dash@fthx"
+      ];
+    };
+  };
 }
