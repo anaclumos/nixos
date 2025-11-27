3fb227011ff727c3bc24da56fdec71f78c2e5f98
Sunghyun Cho <hey@cho.sh>
2025-11-27 13:01:37 +0900

    Remove KakaoTalk from system packages and GNOME configuration
    - Deleted KakaoTalk from the system packages in configuration.nix to streamline the environment.
    - Updated gnome-appearance.nix to exclude KakaoTalk from the list of desktop entries.
    - Adjusted home packages in packages.nix to remove KakaoTalk, ensuring a cleaner package management.

--- flake.nix
+++ flake.nix
@@ -8,29 +8,26 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    kakaotalk.url = "path:/home/sunghyun/Desktop/nix/kakaotalk.nix";
-    affinity-nix.url = "github:mrshmllow/affinity-nix";
   };
 
-  outputs = { self, nixpkgs, home-manager, nixos-hardware, kakaotalk
-    , affinity-nix, ... }@inputs: {
-      nixosConfigurations = {
-        framework = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
-          modules = [
-            { nixpkgs.hostPlatform = "x86_64-linux"; }
-            { nixpkgs.config.allowUnfree = true; }
-            ./configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useUserPackages = true;
-              home-manager.users.sunghyun = import ./home;
-              home-manager.extraSpecialArgs = { inherit inputs; };
-              home-manager.sharedModules =
-                [{ nixpkgs.config.allowUnfree = true; }];
-            }
-          ];
-        };
+  outputs = { self, nixpkgs, home-manager, nixos-hardware, ... }@inputs: {
+    nixosConfigurations = {
+      framework = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          { nixpkgs.hostPlatform = "x86_64-linux"; }
+          { nixpkgs.config.allowUnfree = true; }
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useUserPackages = true;
+            home-manager.users.sunghyun = import ./home;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.sharedModules =
+              [{ nixpkgs.config.allowUnfree = true; }];
+          }
+        ];
       };
     };
+  };
 }
