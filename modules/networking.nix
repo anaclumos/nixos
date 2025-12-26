b8d4bacfda5bf834e7c84d8684f9dd44aa728a91
Sunghyun Cho <hey@cho.sh>
2025-12-26 19:01:39 +0900

    2025-12-26

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }: {
   services.expressvpn.enable = true;
   networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.powersave = false;
+  networking.networkmanager.wifi.backend = "iwd";
+  networking.wireless.iwd.enable = true;
   services.tailscale.enable = true;
   services.adguardhome.enable = true;
 }
